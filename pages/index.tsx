c4f4f3cef9eff9bd6f3df4be50af26e881ff2456
Hieu Truong Quang <hieu@wearebloom.com.au>
2024-05-24 23:32:01 +0700

    Optimize

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,6 @@ export default function Home() {
       <main className="font-mono flex flex-col justify-center items-center  w-screen">
         <h1 className="m-5 text-xl font-bold">Real-Time Object Detection</h1>
         <Yolo />
-        <p className="m-5">
-          Created by{" "}
-          <a
-            className="underline underline-offset-1 hover:translate-y-1"
-            href="https://juanjaho.github.io/"
-          >
-            @juanjaho
-          </a>
-        </p>
       </main>
     </>
   );
